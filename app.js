a1badefd9cc5a0b010f5de3e6197959c22e5fba6
michaellee212 <33797607+michaellee212@users.noreply.github.com>
2019-08-14 23:10:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ var indexRoutes = require("./routes/index");
 //     useNewUrlParser: true
 // });
 
-mongoose.connect("mongodb+srv://michaelee212:Syxz7Cig47FF0guG@cluster0-lek7r.mongodb.net/test?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://michaelee212:Syxz7Cig47FF0guG@cluster0-lek7r.mongodb.net/test?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+});
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({
